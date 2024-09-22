5bf565511d08e5afe73ca2c15bbe5cdc782328de
Andrew N Golovkov <andrew.golovkov@distribusion.com>
2024-09-22 23:42:09 +0300

    weekly-update script

---
+++ freecad-weekly-update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+version=38794
+intel_sha=$(curl -L https://github.com/FreeCAD/FreeCAD-Bundle/releases/download/weekly-builds/FreeCAD_weekly-builds-$version-conda-macOS-x86_64-py311.dmg-SHA256.txt | awk '{print $1}')
+arm64_sha=$(curl -L https://github.com/FreeCAD/FreeCAD-Bundle/releases/download/weekly-builds/FreeCAD_weekly-builds-$version-conda-macOS-arm64-py311.dmg-SHA256.txt | awk '{print $1}')
+
+
+perl -p \
+     -e 's/^(  version \").*/${1}'$version'"/; s/^(  sha256 arm:   ").*$/${1}'$arm64_sha'",/; s/^(         intel: ").*$/${1}'$intel_sha'"/' \
+     -i Casks/freecad-weekly.rb
