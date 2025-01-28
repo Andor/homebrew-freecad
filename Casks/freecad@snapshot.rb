cc771e3e6a99f4fee73e36e67eebc780295a5e68
Andrew N Golovkov <andrew.golovkov@distribusion.com>
2025-01-29 00:10:08 +0200

    40006

--- Casks/freecad@snapshot.rb
+++ Casks/freecad@snapshot.rb
@@ -1,9 +1,9 @@
 cask "freecad@snapshot" do
   arch arm: "arm64", intel: "intel-x86_64"
 
-  version "39980"
-  sha256 arm:   "8ee8724117b05e793c2e15f23110089471be29806bf0d1f721d3e61e38a006c9",
-         intel: "3a351660533ef6f6f05307971802d94617bd561950ff869f9588b3493eeeb205"
+  version "40006"
+  sha256 arm:   "4b171d0d197674a98b9da627f2f31acadabedeefb46a9ec716e2959c0edd076d",
+         intel: "a82398c36cb64cbc75feb3cfaab0ca1b6b4e70528e57432084f09db12255de09"
 
   url "https://github.com/FreeCAD/FreeCAD-Bundle/releases/download/weekly-builds/FreeCAD_weekly-builds-#{version}-conda-macOS-#{arch}-py311.dmg",
       verified: "github.com/FreeCAD/FreeCAD-Bundle/"
