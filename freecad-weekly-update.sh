3bfa26d6c260c3de41e0dd9fe375c8e886bfc074
Andrew N Golovkov <andrew.golovkov@distribusion.com>
2025-03-01 23:48:40 +0200

    improve script to be more automatic

--- freecad-weekly-update.sh
+++ freecad-weekly-update.sh
@@ -2,12 +2,16 @@
 
 set -e -o pipefail
 
-# https://github.com/FreeCAD/FreeCAD-Bundle/releases/expanded_assets/weekly-builds
+url=https://github.com/FreeCAD/FreeCAD-Bundle/releases/expanded_assets/weekly-builds
 
-version=40317
+# FreeCAD_weekly-builds-40444-conda-macOS-arm64-py311.dmg
+build_name=$(curl -s "$url" | grep -oE 'FreeCAD_weekly-builds-[0-9]+-conda-macOS-.*-py[0-9]+\.dmg' | head -1)
+
+version="${build_name#*weekly-builds-}"
+version="${version%%-*}"
+
 intel_sha=$(curl -L https://github.com/FreeCAD/FreeCAD-Bundle/releases/download/weekly-builds/FreeCAD_weekly-builds-$version-conda-macOS-x86_64-py311.dmg-SHA256.txt | awk '{print $1}')
 arm64_sha=$(curl -L https://github.com/FreeCAD/FreeCAD-Bundle/releases/download/weekly-builds/FreeCAD_weekly-builds-$version-conda-macOS-arm64-py311.dmg-SHA256.txt | awk '{print $1}')
-
 
 perl -p \
      -e 's/^(  version \").*/${1}'$version'"/; s/^(  sha256 arm:   ").*$/${1}'$arm64_sha'",/; s/^(         intel: ").*$/${1}'$intel_sha'"/' \
