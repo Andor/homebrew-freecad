733640d24722f4db7ef63d6c38f3774e4fb94f94
Andrew N Golovkov <andrew.golovkov@distribusion.com>
2025-05-17 23:50:12 +0300

    fix version string

--- Casks/freecad@snapshot.rb
+++ Casks/freecad@snapshot.rb
@@ -5,7 +5,7 @@ cask "freecad@snapshot" do
   sha256 arm:   "23f51a8b78a59422b643760ce6ebff9c74a7c849c56fe010a441a684006ba37a",
          intel: "c238c286ad3be8ef42dffdae7dcd4e5b0f48de021e12ea903aa063b21eae4501"
 
-  url "https://github.com/FreeCAD/FreeCAD/releases/download/weekly-#{version}/FreeCAD_weekly-#{version}-macOS-#{arch}-py311.dmg",
+  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version}/FreeCAD_#{version}-macOS-#{arch}-py311.dmg",
     verified: "github.com/FreeCAD/FreeCAD/"
 
   name "FreeCAD"
