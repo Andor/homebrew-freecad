97b23d06513601950c1d60c5b036880117bd14d9
Andrew N Golovkov <andrew.golovkov@distribusion.com>
2025-05-23 21:55:11 +0300

    ignore intel, 2025.05.19

--- Casks/freecad@snapshot.rb
+++ Casks/freecad@snapshot.rb
@@ -2,7 +2,7 @@ cask "freecad@snapshot" do
   arch arm: "arm64", intel: "intel-x86_64"
 
   version "2025.05.19"
-  sha256 arm:   "8a8e3fee813f6a3f9cce2f32558170f52eaffd6edfa21485827d9595a1bc5a1a",
+  sha256 arm:   "8a8e3fee813f6a3f9cce2f32558170f52eaffd6edfa21485827d9595a1bc5a1a"
 
   url "https://github.com/FreeCAD/FreeCAD/releases/download/weekly-#{version}/FreeCAD_weekly-#{version}-macOS-#{arch}-py311.dmg",
     verified: "github.com/FreeCAD/FreeCAD/"
