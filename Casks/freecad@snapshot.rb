a00a7d96dcd9fccd854d8bae3d40b745d1df3465
Andrew N Golovkov <andrew.golovkov@distribusion.com>
2025-05-26 00:13:24 +0300

    fix snapshot from bundle

--- Casks/freecad@snapshot.rb
+++ Casks/freecad@snapshot.rb
@@ -5,7 +5,7 @@ cask "freecad@snapshot" do
   sha256 arm:   "fc86517419acd8d90b6e058e5f63f4f58777fabe406605badc7a65a4051533d0",
          intel: "0ffde156fa21725e42568c2ce190f390db4c39fe5b905d2be2e0596cff850484"
 
-  url "https://github.com/FreeCAD/FreeCAD/releases/download/weekly-#{version}/FreeCAD_weekly-#{version}-macOS-#{arch}-py311.dmg",
+  url "https://github.com/FreeCAD/FreeCAD-Bundle/releases/download/weekly-builds/FreeCAD_weekly-builds-#{version}-conda-macOS-#{arch}-py311.dmg",
     verified: "github.com/FreeCAD/FreeCAD/"
 
   name "FreeCAD"
