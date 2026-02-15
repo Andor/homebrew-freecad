6b5fe4edde50b4249760b1e2dafebb846f178267
Andrew N Golovkov <andrew.golovkov@gmail.com>
2026-02-15 14:01:39 +0200

    urls were changed as well

--- Casks/freecad@weekly.rb
+++ Casks/freecad@weekly.rb
@@ -4,14 +4,15 @@ cask "freecad@weekly" do
   on_arm do
     version "2026.02.11"
     sha256 "c5d22542f3b5c6edf98077b08345095a05bda9b2004b176e24ca23d074cc703a"
+    url "https://github.com/FreeCAD/FreeCAD/releases/download/weekly-#{version}/FreeCAD_weekly-#{version}-macOS-#{arch}-macOS15-py311.dmg",
+      verified: "github.com/FreeCAD/FreeCAD/"
   end
   on_intel do
     version "2026.02.11"
     sha256 "7e6afbe0a24913b115ae57aaa13c0c012b0e258eb1736d596c69f46fb6eebbdc"
+    url "https://github.com/FreeCAD/FreeCAD/releases/download/weekly-#{version}/FreeCAD_weekly-#{version}-macOS-#{arch}-macOS10-py311.dmg",
+      verified: "github.com/FreeCAD/FreeCAD/"
   end
-
-  url "https://github.com/FreeCAD/FreeCAD/releases/download/weekly-#{version}/FreeCAD_weekly-#{version}-macOS-#{arch}-py311.dmg",
-    verified: "github.com/FreeCAD/FreeCAD/"
 
   name "FreeCAD"
   desc "3D parametric modeller"
