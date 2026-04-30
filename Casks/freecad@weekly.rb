1761c107204f93b5cacf0a5177c9af61669297aa
Andrew N Golovkov <andrew.golovkov@gmail.com>
2026-04-30 15:33:52 +0300

    2026.04.29

--- Casks/freecad@weekly.rb
+++ Casks/freecad@weekly.rb
@@ -2,14 +2,14 @@ cask "freecad@weekly" do
   arch arm: "arm64", intel: "intel-x86_64"
 
   on_arm do
-    version "2026.04.22"
-    sha256 "5356f46dc605737d829bf11232feed13ee7e8be979d1f6d0b433619ea8858e40"
+    version "2026.04.29"
+    sha256 "8f507d2ec20b38117a023d54bac2aa71fafeadf3b7db56d1bf4fe73b7f09f269"
     url "https://github.com/FreeCAD/FreeCAD/releases/download/weekly-#{version}/FreeCAD_weekly-#{version}-macOS11-#{arch}.dmg",
       verified: "github.com/FreeCAD/FreeCAD/"
   end
   on_intel do
-    version "2026.04.22"
-    sha256 "9fd143b160d5fffa8d192941774106024b7eab3c0c6e9ff59d8c595e417e0e51"
+    version "2026.04.29"
+    sha256 "a4bc2edee6fa7ed54be55e0dc4d8dff2700bb43580a0fb48cf32aa0060e83363"
     url "https://github.com/FreeCAD/FreeCAD/releases/download/weekly-#{version}/FreeCAD_weekly-#{version}-macOS10-#{arch}.dmg",
       verified: "github.com/FreeCAD/FreeCAD/"
   end
